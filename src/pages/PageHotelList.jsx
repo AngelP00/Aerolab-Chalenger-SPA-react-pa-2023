0d269ddafa2fab8f53d9d70c188a76d6016007f4
unknown <angelpalacios01234@gmail.com>
2023-11-13 23:46:54 -0300

    [UPDATE]: PageHotelList.jsx getData.js

--- src/pages/PageHotelList.jsx
+++ src/pages/PageHotelList.jsx
@@ -15,6 +15,12 @@ import Typography from "@mui/material/Typography";
 //start aerolab
 import { useEffect, useState } from "react";
 import getData from "../utils/getData";
+
+import {Paper, IconButton} from '@mui/material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useLocation } from 'react-router-dom';
+//import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 //end aerolab
 
 const fetchHotels = async () => {
@@ -26,6 +32,7 @@ const fetchHotels = async () => {
 };
 
 const PageHotelList = () => {
+	const navigate = useNavigate();
 	//start aerolab
 	/*
 	const [data, setData] = useState([]);
@@ -39,12 +46,28 @@ const PageHotelList = () => {
 	//end aerolab
 
 	console.log('Inicio HotelList a');
+	const location = useLocation();
+
+  	const queryParams = new URLSearchParams(location.search);
+  	const filtro = queryParams.get('productsorder');
+	console.log('filtro: ',filtro);
+
+	const [sortType, setSortType] = useState(filtro);
+	/*
 	const {
 		data: products,
 		isLoading,
 		error,
 	//} = useQuery({ queryKey: ["allProducts"], queryFn: fetchHotels });
-	} = useQuery({ queryKey: ["allProducts"], queryFn: getData });
+	} = useQuery({ queryKey: ["allProducts"], queryFn: getData(sortType) });
+	*/
+	//filter mostRecent, lowToHigh, highToLow
+	const {
+		data: products,
+		isLoading,
+		error,
+	  } = useQuery(["allProducts", sortType], () => getData(sortType));
+	  
 	console.log('Inicio HotelList b');
 	if (isLoading) {
 		//return <div>Loading...</div>;
@@ -53,6 +76,7 @@ const PageHotelList = () => {
 			</Typography>;
 	}
 	console.log('Inicio HotelList c');
+	console.log('products:',products);
 
 	if (error) {
 		//return <div>Error fetching Hotels! {error.message}</div>;
@@ -60,6 +84,105 @@ const PageHotelList = () => {
 				Error fetching Hotels! {error.message}
 			</Typography>;
 	}
+
+
+	//inicio filtrar
+	//const handleSort = (type) => {
+		//console.log("handleSort");
+		// Actualiza el tipo de orden y vuelve a renderizar la lista de productos
+		//setSortType(type);
+	
+		// Aquí deberías ordenar la lista de productos según el tipo seleccionado (type)
+		// y luego actualizar el estado de los productos (setProducts) con la nueva lista ordenada.
+		// Ejemplo (puedes utilizar tu propia lógica de ordenación):
+		/*
+		let sortedProducts = [...products];
+		if (type === 'lowToHigh') {
+		  sortedProducts.sort((a, b) => a.cost - b.cost);
+		} else if (type === 'highToLow') {
+		  sortedProducts.sort((a, b) => b.cost - a.cost);
+		} else if (type === 'latest') {
+		  sortedProducts.sort((a, b) => new Date(b.date) - new Date(a.date));
+		}
+		console.log("sortedProducts:",sortedProducts);
+		 */
+		//setProducts(sortedProducts);
+		//products = sortedProducts;
+	  //};
+	//fin filtrar
+
+	//const history = useHistory();
+
+	const handleButtonClick = (productsorder) => {
+		// Aquí puedes realizar cualquier lógica adicional antes de cambiar la URL
+		//setSortType(productsorder);
+
+		// Modificar la URL con el nuevo parámetro
+		/*
+		history.push({
+		pathname: window.location.pathname, // Mantener la ruta actual
+		search: `?productsorder=${productsorder}`, // Agregar o modificar el parámetro de consulta
+		});
+		*/
+		// Redirige a la misma página con el filtro de orden en la URL
+    	navigate(`?productsorder=${productsorder}`);
+		console.log("hola23");
+		//hay que agregarle esto ya que con la linea de arriba se cambia la url del navegador pero la que usa react np se cambia, para que funcione tiene que haber un refresco de pagina (dando refrsh a la pagina o pasar a otra pagina y volver)
+		setSortType(productsorder);
+	};
+
+	const buttonStyleDeselected = {
+		background: '#ededed',
+		borderRadius: '100px',
+		width: '100%',
+		height: '48px',
+		padding: '5px',
+		marginBottom: '10px',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		'&:focus': {
+		  outline: 'none',
+		},
+		'&:active': {
+		  boxShadow: 'none',
+		},
+	};
+	const buttonStyleSelected = {
+		background: '#0ad4fa',
+		borderRadius: '100px',
+		width: '100%',
+		height: '48px',
+		padding: '5px',
+		marginBottom: '10px',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		'&:focus': {
+		  outline: 'none',
+		},
+		'&:active': {
+		  boxShadow: 'none',
+		},
+	  };
+	
+	const buttonTextStyleDeselected = {
+		fontFamily: 'SourceSansPro-Regular',
+		fontSize: '20px',
+		color: '#a3a3a3',
+		letterSpacing: '-0.15px',
+		lineHeight: '48px',
+		textAlign: 'center',
+	};
+	const buttonTextStyleSelected = {
+		fontFamily: 'SourceSansPro-Regular',
+		fontSize: '20px',
+		color: '#ffffff',
+		letterSpacing: '-0.15px',
+		lineHeight: '48px',
+		textAlign: 'center',
+	};
+
 	console.log('Final HotelList');
 	return (
 		<>
@@ -94,6 +217,124 @@ const PageHotelList = () => {
 				Electronics
 			</Typography>
 			</div>
+
+			
+
+			<Box
+			display="flex"
+			flexDirection={{ xs: 'column', sm: 'row' }}
+			justifyContent="space-between"
+			alignItems={{ xs: 'flex-start', sm: 'center' }}
+			paddingInline={{ xs: '15px', sm: '60px' }}
+			paddingTop="15px"
+			paddingBottom="15px"
+			>
+				
+				<Box
+      display="flex"
+      flexDirection={{ xs: 'column', sm: 'row' }}
+      alignItems="center"
+      marginBottom={{ xs: '15px', sm: '0' }}
+    >
+		<Box
+				display="flex"
+				alignItems="center"
+				marginBottom={{ xs: '15px', sm: '0' }}
+				>
+				<Typography
+				variant="body2"
+				style={{
+					fontFamily: 'SourceSansPro-Regular',
+					fontSize: '24px',
+					color: '#616161',
+					letterSpacing: '-0.15px',
+					lineHeight: '48px',
+					textAlign: 'left',
+				}}
+				>
+				{products.length} de {products.length} productos
+				</Typography>
+				<div
+				style={{
+					background: '#d9d9d9',
+					width: '1px',
+					height: '30px',
+					marginInline: '24px',
+				}}
+				></div>
+
+				<Typography
+				variant="body2"
+				style={{
+					fontFamily: 'SourceSansPro-Regular',
+					fontSize: '24px',
+					color: '#a3a3a3',
+					letterSpacing: '-0.15px',
+					lineHeight: '48px',
+					textAlign: 'left',
+				}}
+				>
+				Ordenar por:
+				</Typography>
+		</Box>
+      <Button
+        variant="contained"
+        disableFocusRipple
+		onClick={() => handleButtonClick('mostRecent')}
+		sx={sortType == "mostRecent" ? buttonStyleSelected : buttonStyleDeselected}
+      >
+        <Typography
+          variant="body2"
+		  sx={sortType == "mostRecent" ? buttonTextStyleSelected : buttonTextStyleDeselected}
+        >
+          Menor Precio
+        </Typography>
+      </Button>
+
+      <Button
+        variant="contained"
+        disableFocusRipple
+		onClick={() => handleButtonClick('lowToHigh')}
+		sx={sortType == "lowToHigh" ? buttonStyleSelected : buttonStyleDeselected}
+      >
+        <Typography
+          variant="body2"
+		  sx={sortType == "lowToHigh" ? buttonTextStyleSelected : buttonTextStyleDeselected}
+        >
+          Menor Precio
+        </Typography>
+      </Button>
+
+      <Button
+        variant="contained"
+        disableFocusRipple
+		onClick={() => handleButtonClick('highToLow')}
+		sx={sortType == "highToLow" ? buttonStyleSelected : buttonStyleDeselected}
+      >
+        <Typography
+          variant="body2"
+		  sx={sortType == "highToLow" ? buttonTextStyleSelected : buttonTextStyleDeselected}
+        >
+          Mayor Precio
+        </Typography>
+      </Button>
+    </Box>
+			<Box>
+				<IconButton
+				sx={{
+					borderRadius: '50%',
+					backgroundColor: '#fff',
+					border: '1px solid #ccc',
+					marginLeft: { xs: '0', sm: '10px' },
+				}}
+				>
+				<ArrowForwardIcon />
+				</IconButton>
+			</Box>
+			</Box>;
+			
+			
+
 			
 			<Grid container spacing={2} textAlign="-webkit-center" sx={{ marginTop: "0px", backgroundColor: "#f9f9f9" }}>{/* backgroundColor: "#f9f9f9"*/}
 				{products.map((hotel) => (
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -6,12 +6,41 @@ const HEADERS = {
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzFjMzFjMDFmMWY0NTAwMjEwOTliNjAiLCJpYXQiOjE2NjI3OTIxMjl9.rNPq16e0LRduvU98shcQ8zKjKAqbOpAQKtH6oL_x10g",
 };
 
-const getData = () => {
+const getDataAnterior = () => {
   return fetch(URL, {
     method: "GET",
     headers: HEADERS,
   }).then((res) => res.json());
 };
+const getData2 = (filter) => {
+  console.log("ordenar por: ",filter);
+  
+  return fetch(URL, {
+    method: "GET",
+    headers: HEADERS,
+  }).then((res) => res.json());
+};
+const getData = (filter) => {
+  console.log("ordenar por: ", filter);
+  //filter mostRecent, lowToHigh, highToLow
+  return fetch(URL, {
+    method: "GET",
+    headers: HEADERS,
+  })
+  .then((res) => res.json())
+  .then((data) => {
+    // Aplicar el filtrado en el lado del cliente según el parámetro
+    if (filter === 'lowToHigh') {
+      return data.sort((a, b) => a.cost - b.cost);
+    } else if (filter === 'highToLow') {
+      return data.sort((a, b) => b.cost - a.cost);
+    } else {
+      return data; // No aplicar ningún filtro
+    }
+  });
+};
+
+
 
 export const getUserData = () => {
   const userUrl = "https://coding-challenge-api.aerolab.co/user/me";
