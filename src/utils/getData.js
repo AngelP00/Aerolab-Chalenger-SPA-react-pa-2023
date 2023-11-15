c949a9aef69ed6bda1d002ba582dedd6e65c6050
unknown <angelpalacios01234@gmail.com>
2023-11-15 01:43:23 -0300

    [UPDATE]: getData.js

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -41,31 +41,4 @@ const getData = (filter) => {
 };
 
 
-
-export const getUserData = () => {
-  const userUrl = "https://coding-challenge-api.aerolab.co/user/me";
-  return fetch(userUrl, {
-    method: "GET",
-    headers: HEADERS,
-  }).then((res) => res.json());
-};
-
-export const updatePoints = (amount) => {
-  const postUserUrl = "https://coding-challenge-api.aerolab.co/user/points";
-  return fetch(postUserUrl, {
-    method: "POST",
-    body: JSON.stringify({ "amount": amount }),
-    headers: HEADERS,
-  });
-};
-
-export const addItemToHistory = (id) => {
-  const postReedemURL = "https://coding-challenge-api.aerolab.co/redeem";
-  return fetch(postReedemURL, {
-    method: "POST",
-    body: JSON.stringify({ productId: id }),
-    headers: HEADERS,
-  });
-};
-
 export default getData;
