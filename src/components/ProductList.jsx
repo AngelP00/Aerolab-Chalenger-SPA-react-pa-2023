f3cee8bdb360f651e82e921cad4ecd226163e766
unknown <angelpalacios01234@gmail.com>
2023-11-15 18:10:09 -0300

    [UPDATE]: ProductList.jsx PageProductDetails.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 //import { Link } from "wouter";
 import {Link } from "react-router-dom";
-
 import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Grid";
 import { Box } from "@mui/material";
@@ -11,39 +10,16 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-
-//start aerolab
 import { useEffect, useState } from "react";
 import getData from "../utils/getData";
-
 import {Paper, IconButton} from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useLocation } from 'react-router-dom';
 //import { useHistory } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-//end aerolab
-
-const fetchHotels = async () => {
-	const response = await fetch("http://localhost:3001/hotels");
-	if (!response.ok) {
-		throw new Error("Network response was not ok");
-	}
-	return response.json();
-};
 
 const PageHotelList = () => {
 	const navigate = useNavigate();
-	//start aerolab
-	/*
-	const [data, setData] = useState([]);
-	useEffect(() => {
-		getData().then((data) => {
-		  setData(data);
-		});
-	}, []);
-	console.log('data: ', data);
-	*/
-	//end aerolab
 
 	console.log('Inicio HotelList a');
 	const location = useLocation();
@@ -53,14 +29,7 @@ const PageHotelList = () => {
 	console.log('filtro: ',filtro);
 
 	const [sortType, setSortType] = useState(filtro != null ? filtro: "mostRecent");
-	/*
-	const {
-		data: products,
-		isLoading,
-		error,
-	//} = useQuery({ queryKey: ["allProducts"], queryFn: fetchHotels });
-	} = useQuery({ queryKey: ["allProducts"], queryFn: getData(sortType) });
-	*/
+	
 	//filter mostRecent, lowToHigh, highToLow
 	const {
 		data: products,
@@ -76,7 +45,6 @@ const PageHotelList = () => {
 			</Typography>;
 	}
 	console.log('Inicio HotelList c');
-	console.log('products:',products);
 
 	if (error) {
 		//return <div>Error fetching Hotels! {error.message}</div>;
@@ -85,49 +53,11 @@ const PageHotelList = () => {
 			</Typography>;
 	}
 
-
-	//inicio filtrar
-	//const handleSort = (type) => {
-		//console.log("handleSort");
-		// Actualiza el tipo de orden y vuelve a renderizar la lista de productos
-		//setSortType(type);
-	
-		// Aquí deberías ordenar la lista de productos según el tipo seleccionado (type)
-		// y luego actualizar el estado de los productos (setProducts) con la nueva lista ordenada.
-		// Ejemplo (puedes utilizar tu propia lógica de ordenación):
-		/*
-		let sortedProducts = [...products];
-		if (type === 'lowToHigh') {
-		  sortedProducts.sort((a, b) => a.cost - b.cost);
-		} else if (type === 'highToLow') {
-		  sortedProducts.sort((a, b) => b.cost - a.cost);
-		} else if (type === 'latest') {
-		  sortedProducts.sort((a, b) => new Date(b.date) - new Date(a.date));
-		}
-		console.log("sortedProducts:",sortedProducts);
-		 */
-		//setProducts(sortedProducts);
-		//products = sortedProducts;
-	  //};
-	//fin filtrar
-
-	//const history = useHistory();
-
 	const handleButtonClick = (productsorder) => {
 		// Aquí puedes realizar cualquier lógica adicional antes de cambiar la URL
-		//setSortType(productsorder);
-
-		// Modificar la URL con el nuevo parámetro
-		/*
-		history.push({
-		pathname: window.location.pathname, // Mantener la ruta actual
-		search: `?productsorder=${productsorder}`, // Agregar o modificar el parámetro de consulta
-		});
-		*/
 		// Redirige a la misma página con el filtro de orden en la URL
     	navigate(`?productsorder=${productsorder}`);
-		console.log("hola23");
-		//hay que agregarle esto ya que con la linea de arriba se cambia la url del navegador pero la que usa react np se cambia, para que funcione tiene que haber un refresco de pagina (dando refrsh a la pagina o pasar a otra pagina y volver)
+		//hay que agregarle esto ya que con la linea de arriba se cambia la url del navegador pero la que usa react no se cambia, para que funcione tiene que haber un refresco de pagina (dando refrsh a la pagina o pasar a otra pagina y volver)
 		setSortType(productsorder);
 	};
 
@@ -186,9 +116,6 @@ const PageHotelList = () => {
 	console.log('Final HotelList');
 	return (
 		<>
-
-			
-
 			<Box
 			display="flex"
 			flexDirection={{ xs: 'column', sm: 'row' }}
@@ -200,111 +127,122 @@ const PageHotelList = () => {
 			>
 				
 				<Box
-      display="flex"
-      flexDirection={{ xs: 'column', sm: 'row' }}
-      alignItems="center"
-      marginBottom={{ xs: '15px', sm: '0' }}
-    >
-		<Box
 				display="flex"
+				flexDirection={{ xs: 'column', sm: 'row' }}
 				alignItems="center"
 				marginBottom={{ xs: '15px', sm: '0' }}
 				>
-				<Typography
-				variant="body2"
-				style={{
-					fontFamily: 'SourceSansPro-Regular',
-					fontSize: '24px',
-					color: '#616161',
-					letterSpacing: '-0.15px',
-					lineHeight: '48px',
-					textAlign: 'left',
-				}}
-				>
-				{products.length} de {products.length} productos
-				</Typography>
-				<div
-				style={{
-					background: '#d9d9d9',
-					width: '1px',
-					height: '30px',
-					marginInline: '24px',
-				}}
-				></div>
+					<Box
+					display="flex"
+					alignItems="center"
+					marginBottom={{ xs: '15px', sm: '0' }}
+					>
+					<Typography
+						variant="body2"
+						style={{
+						fontFamily: 'SourceSansPro-Regular',
+						fontSize: '24px',
+						color: '#616161',
+						letterSpacing: '-0.15px',
+						lineHeight: '48px',
+						textAlign: 'left',
+						whiteSpace: 'nowrap', // Añadida para mantener el texto en una línea
+						}}
+					>
+						{products.length} de {products.length} productos
+					</Typography>
+					<div
+						style={{
+						background: '#d9d9d9',
+						width: '1px',
+						height: '30px',
+						marginInline: '24px',
+						}}
+					></div>
+					<Typography
+						variant="body2"
+						style={{
+						fontFamily: 'SourceSansPro-Regular',
+						fontSize: '24px',
+						color: '#a3a3a3',
+						letterSpacing: '-0.15px',
+						lineHeight: '48px',
+						textAlign: 'left',
+						whiteSpace: 'nowrap', // Añadida para mantener el texto en una línea
+						}}
+					>
+						Ordenar por:
+					</Typography>
+					</Box>
 
-				<Typography
-				variant="body2"
-				style={{
-					fontFamily: 'SourceSansPro-Regular',
-					fontSize: '24px',
-					color: '#a3a3a3',
-					letterSpacing: '-0.15px',
-					lineHeight: '48px',
-					textAlign: 'left',
-				}}
-				>
-				Ordenar por:
-				</Typography>
-		</Box>
-      <Button
-        variant="contained"
-        disableFocusRipple
-		onClick={() => handleButtonClick('mostRecent')}
-		sx={sortType == "mostRecent" ? buttonStyleSelected : buttonStyleDeselected}
-      >
-        <Typography
-          variant="body2"
-		  sx={sortType == "mostRecent" ? buttonTextStyleSelected : buttonTextStyleDeselected}
-        >
-			Most recent
-        </Typography>
-      </Button>
 
-      <Button
-        variant="contained"
-        disableFocusRipple
-		onClick={() => handleButtonClick('lowToHigh')}
-		sx={sortType == "lowToHigh" ? buttonStyleSelected : buttonStyleDeselected}
-      >
-        <Typography
-          variant="body2"
-		  sx={sortType == "lowToHigh" ? buttonTextStyleSelected : buttonTextStyleDeselected}
-        >
-			Lowest price
-        </Typography>
-      </Button>
+					<Button
+					variant="contained"
+					disableFocusRipple
+					onClick={() => handleButtonClick('mostRecent')}
+					sx={sortType === "mostRecent" ? buttonStyleSelected : buttonStyleDeselected}
+					>
+					<Typography
+						variant="body2"
+						sx={{
+						whiteSpace: 'nowrap', // Añadida para mantener el texto en una línea
+						...((sortType === "mostRecent") ? buttonTextStyleSelected : buttonTextStyleDeselected),
+						}}
+					>
+						Most recent
+					</Typography>
+					</Button>
 
-      <Button
-        variant="contained"
-        disableFocusRipple
-		onClick={() => handleButtonClick('highToLow')}
-		sx={sortType == "highToLow" ? buttonStyleSelected : buttonStyleDeselected}
-      >
-        <Typography
-          variant="body2"
-		  sx={sortType == "highToLow" ? buttonTextStyleSelected : buttonTextStyleDeselected}
-        >
-			Highest price
-        </Typography>
-      </Button>
-    </Box>
-			<Box>
-				<IconButton
-				sx={{
-					borderRadius: '50%',
-					backgroundColor: '#fff',
-					border: '1px solid #ccc',
-					marginLeft: { xs: '0', sm: '10px' },
-				}}
-				>
-				<ArrowForwardIcon />
-				</IconButton>
-			</Box>
+					<Button
+					variant="contained"
+					disableFocusRipple
+					onClick={() => handleButtonClick('lowToHigh')}
+					sx={sortType === "lowToHigh" ? buttonStyleSelected : buttonStyleDeselected}
+					>
+					<Typography
+						variant="body2"
+						sx={{
+						whiteSpace: 'nowrap', // Añadida para mantener el texto en una línea
+						...((sortType === "lowToHigh") ? buttonTextStyleSelected : buttonTextStyleDeselected),
+						}}
+					>
+						Lowest price
+					</Typography>
+					</Button>
+
+					<Button
+					variant="contained"
+					disableFocusRipple
+					onClick={() => handleButtonClick('highToLow')}
+					sx={sortType === "highToLow" ? buttonStyleSelected : buttonStyleDeselected}
+					>
+					<Typography
+						variant="body2"
+						sx={{
+						whiteSpace: 'nowrap', // Añadida para mantener el texto en una línea
+						...((sortType === "highToLow") ? buttonTextStyleSelected : buttonTextStyleDeselected),
+						}}
+					>
+						Highest price
+					</Typography>
+					</Button>
+
+					
+					</Box>
+					<Box>
+					<IconButton
+					sx={{
+						borderRadius: '50%',
+						backgroundColor: '#fff',
+						border: '1px solid #ccc',
+						marginLeft: { xs: '0', sm: '10px' },
+					}}
+					>
+						<ArrowForwardIcon />
+					</IconButton>
+				</Box>
 			</Box>;
-			
-			
 
-			
 			<Grid container spacing={2} textAlign="-webkit-center" sx={{ marginTop: "0px", backgroundColor: "#f9f9f9" }}>{/* backgroundColor: "#f9f9f9"*/}
 				{products.map((product) => (
 					<Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
@@ -342,43 +280,6 @@ const PageHotelList = () => {
 					</Grid>
 				))}
 			</Grid>
-			{/*
-			<Grid container spacing={2} textAlign="-webkit-center" sx={{ marginTop: "20px" }}>
-				{hotels.map((product) => (
-					<Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-					<Link to={`/product/${product.id}`} style={{ textDecoration: "none" }}>
-						<Card sx={{
-							maxWidth: 345, // Ancho máximo fijo para todas las tarjetas
-							backgroundColor: "#e8e8e8",
-							//display: "flex",
-							
-							height: "100%", // Altura fija para el contenedor de la tarjeta
-							//width: "100%", // Ancho fijo del 100%
-							//justifyContent: "space-between", // Alinea los elementos en el eje principal
-							//flexDirection: "column",
-							}}>
-						<Box sx={{ height: 285, display: "flex", flexDirection: "column" }}>
-						<CardMedia sx={{ height: 140 }} image={product.image} title={product.name} />
-						<CardContent>
-							<Typography gutterBottom variant="h5" component="div">
-								{product.name}
-							</Typography>
-							<Typography variant="body2" color="text.secondary">
-								{product.description.length > 130
-								? `${product.description.slice(0, 130)}...`
-								: product.description}
-							</Typography>
-						</CardContent>
-						</Box>
-						<CardActions sx={{ display: "flex", flexDirection: "column", alignItems: "stretch" }}>
-							<Button size="small">See Details</Button>
-						</CardActions>
-						</Card>
-					</Link>
-					</Grid>
-				))}
-			</Grid>
-			*/}
 		</>
 		
 	  );
--- src/pages/PageProductDetails.jsx
+++ src/pages/PageProductDetails.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 //import { useRoute,Link, useLocation } from "wouter";
 import { useParams,Link } from "react-router-dom";
-import BookingForm from "../components/BookingForm";
+//import BookingForm from "../components/BookingForm";
 
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -11,65 +11,29 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
-//start aerolab
 import { useEffect, useState } from "react";
 import getData from "../utils/getData";
-//end aerolab
 
-/*
-const fetchHotel = async (id) => {
-		console.log('obteniendo data del product ',id)
-		const response = await fetch(`http://localhost:3001/hotels/${id}`);
-		if (!response.ok) {
-			throw new Error("Network response was not ok");
+const fetchProduct = async (id) => {
+	console.log('Obteniendo datos del product', id);
+	
+	// Llama a una función 'getData' que debe devolver la lista de productos
+	return getData().then((data) => {
+		console.log('Datos obtenidos:', data);
+		const found = data.find(item => item._id === id);
+	
+		if (found) {
+		console.log('Producto encontrado:', found);
+		return found; // Devuelve el producto encontrado
+		} else {
+		console.log(`Producto con ID ${id} no encontrado.`);
+		return null; // Devuelve null si no se encuentra el producto
 		}
-		return response.json();
-	};
-*/
-
-	const fetchProduct = async (id) => {
-		console.log('Obteniendo datos del product', id);
-	  
-		// Llama a una función 'getData' que debe devolver la lista de productos
-		return getData().then((data) => {
-		  console.log('Datos obtenidos:', data);
-		  const found = data.find(item => item._id === id);
-	  
-		  if (found) {
-			console.log('Producto encontrado:', found);
-			return found; // Devuelve el producto encontrado
-		  } else {
-			console.log(`Producto con ID ${id} no encontrado.`);
-			return null; // Devuelve null si no se encuentra el producto
-		  }
-		});
-	  };
+	});
+};
 	  
 
 const PageHotelDetails = () => {
-	console.log('HotelDetails');
-	//start aerolab
-	/*
-	const { id } = useParams();
-	const [product, setProduct] = useState(null);
-	useEffect(() => {
-		getData().then((data) => {
-			console.log('useEffect getData 1');
-		  const found = data.find(item => item._id == id);
-		  setProduct(found)
-		  console.log('useEffect getData 2');
-		  console.log('product: ', found);
-		});
-	
-	}, [id]);
-	
-	  if (!product) {
-		return <div></div>;
-	  }
-	*/
-	
-	//end aerolab
-
 	console.log('HotelDetails');
 	
 	//const [match, params] = useRoute("/product/:id");
@@ -111,67 +75,31 @@ const PageHotelDetails = () => {
 	return (
 		<div style={{ display: "flex", justifyContent: "center", alignItems: "flex-start", height: "100vh", margin: "20px" }}>
 			<Card sx={{ backgroundColor: "#e8e8e8" }}>
-
-
 				<Box sx={{display: "flex", flexDirection: "column" }}>
-						<CardMedia
-							sx={{ height: "auto" }}
-							component="img"
-							image={product.img?.hdUrl}
-							title={product.name}
-						/>
-						<CardContent>
-							<Typography gutterBottom variant="h5" component="div">
-								{product.name}
-							</Typography>
-							<Typography variant="body2" color="text.secondary" sx={{ maxWidth: "400px" }}>
-								Category: {product.category}
-							</Typography>
-							<Typography variant="body2" color="text.secondary" sx={{ maxWidth: "400px" }}>
-								Cost: ${product.cost} USD
-							</Typography>
-
-						</CardContent>
+					<CardMedia
+						sx={{ height: "auto" }}
+						component="img"
+						image={product.img?.hdUrl}
+						title={product.name}
+					/>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="div">
+							{product.name}
+						</Typography>
+						<Typography variant="body2" color="text.secondary" sx={{ maxWidth: "400px" }}>
+							Category: {product.category}
+						</Typography>
+						<Typography variant="body2" color="text.secondary" sx={{ maxWidth: "400px" }}>
+							Cost: ${product.cost} USD
+						</Typography>
+					</CardContent>
 				</Box>
 
-
-				
 				<CardActions>
 				{/*<BookingForm /> */}
 				</CardActions>
 			</Card>
 		</div>
-		/*
-		<div style={{ display: "flex", justifyContent: "center", alignItems: "flex-start", height: "100vh", margin: "20px" }}>
-			<Card sx={{ backgroundColor: "#e8e8e8" }}>
-
-
-				<Box sx={{display: "flex", flexDirection: "column" }}>
-						<CardMedia
-							sx={{ height: "auto" }}
-							component="img"
-							image={product.image}
-							title={product.name}
-						/>
-						<CardContent>
-							<Typography gutterBottom variant="h5" component="div">
-								{product.name}
-							</Typography>
-							<Typography variant="body2" color="text.secondary" sx={{ maxWidth: "400px" }}>
-								{product.description}
-							</Typography>
-
-						</CardContent>
-				</Box>
-
-
-				
-				<CardActions>
-				<BookingForm />
-				</CardActions>
-			</Card>
-		</div>
-		*/
 	);
 };
 
